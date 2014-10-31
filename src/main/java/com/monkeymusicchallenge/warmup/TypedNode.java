7e0d1d8dca6cfb101c1830350d40442eae0fff9b
izimbra <izimbra@izimbp.local>
2014-10-31 09:52:37 +0100

    Graph node class

---
+++ src/main/java/com/monkeymusicchallenge/warmup/TypedNode.java
@@ -0,0 +1,30 @@
+package com.monkeymusicchallenge.warmup;
+
+import java.util.LinkedList;
+
+/**
+ * Graph node of specific type with 2D coordinates
+ */
+public class TypedNode {
+    private int x, y;
+    private int type;
+
+    public TypedNode (int x, int y, int type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+}
