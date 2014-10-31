eda3fc1644d6915d60bb66c890175e704c8f8ab7
izimbra <izimbra@izimbp.local>
2014-10-31 09:53:23 +0100

    Graph class

--- src/main/java/com/monkeymusicchallenge/warmup/Graph.java
+++ src/main/java/com/monkeymusicchallenge/warmup/Graph.java
@@ -2,6 +2,7 @@ package com.monkeymusicchallenge.warmup;
 
 import java.util.LinkedList;
 
+
 /**
  * Created by izimbra on 30/10/14.
  * Rip-off of Sedgewick & Wayne's (2011, p.526) Graph class
@@ -10,35 +11,35 @@ import java.util.LinkedList;
 public class Graph {
     private final int V; // no. vertices
     private int E; // no. edges
-    public LinkedList<Integer>[] adj; // adjacency lists to represent connected nodes
+    private TypedNode[] nodes; // nodes of the graph
+    private LinkedList<Integer> adj[];
 
     public Graph(int V) {
         this.V = V;
+        nodes = new TypedNode[V];
         adj = (LinkedList<Integer>[]) new LinkedList[V];
-        for (LinkedList<Integer> a : adj) {
+        for (LinkedList<Integer> a : adj)
             a = new LinkedList<Integer>();
-        }
     }
 
-    // Converts node coordinates to index
+/*    // Converts node coordinates to index
     public int xyToInd(int x, int y) {
 
+    }*/
+
+    // Add or update i-th node of the graph
+    public void addNode(int i, TypedNode n) {
+        nodes[i] = n;
     }
 
+    // Adds an edge between two nodes
     public void addEdge(int v, int w) {
-
-    }
-
-    /**
-     * Helper class
-     * Graph vertex with 2D coordinates
-     */
-    private static class Node {
-        private int x, y;
-        private int kind;
-
-        public Node (int x, int y, int kind) {
-
+        if (nodes[v] == null | nodes [w] == null)
+            throw new NullPointerException("Node not initialised");
+        else {
+            adj[v].add(w);
+            E++;
         }
     }
+
 }
