205722a13b019f3fd39539f645af9982c2688101
izimbra <izimbra@dhcp-047233.eduroam.chalmers.se>
2014-11-03 11:40:53 +0100

    Tests

--- test/main/java/com/monkeymusicchallenge/warmup/GraphTest.java
+++ test/main/java/com/monkeymusicchallenge/warmup/GraphTest.java
@@ -1,10 +1,38 @@
 package com.monkeymusicchallenge.warmup;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class GraphTest {
 
+    private Graph g;
+
+    /** Create the simple graph
+     *
+     *       2
+     *       ^
+     *       |
+     * 0 - > 1
+     */
     @Before
-    public void SetUp() {}
+    public void setUp() {
+        g = new Graph(3);
+        g.addEdge(0,1);
+        g.addEdge(1,2);
+    }
+
+    @Test
+    public void noOfVertices() {
+        assertEquals(g.nrOfVertices(), 3);
+    }
+
+    @Test
+    public void adjListsNotEmpty() {
+        for (int i = 0; i < g.nrOfVertices(); i++)
+            assertNotNull(g.adj(i));
+    }
 
 }
