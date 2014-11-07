bb6cb1d3afd92ae1e3f0f67107aba8468c0dfa2a
Patrik Nygren <patriknygren82@gmail.com>
2014-11-08 00:54:03 +0100

    some more cleaning

--- src/main/java/com/monkeymusicchallenge/warmup/TypedNode.java
+++ src/main/java/com/monkeymusicchallenge/warmup/TypedNode.java
@@ -7,21 +7,17 @@ import java.util.LinkedList;
  */
 public class TypedNode {
     private int x, y;
-    private boolean visited; 
     private int type;
 
     public TypedNode (int x, int y, int type) {
         this.x = x;
         this.y = y;
-        this.visited = false;
         this.type = type;
     }
 
     public int getX() { return x; }
     public int getY() { return y; }
     public int getType() { return type; }   
-    public void visit(boolean mark) { this.visited = mark; }    
-    public boolean visited() { return this.visited; }
     
     public TypedNode clone() {
     	return new TypedNode(this.x, this.y, this.type);
